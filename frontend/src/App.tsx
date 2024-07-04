5d81bb9756a1911caeb504271d42e13338cf7ad8
Facundo Pettersson <petterssonfacundo@gmail.com>
2024-07-04 20:00:55 -0300

    add: OpenApiGeneretor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-
+      <h1>Bienvenido</h1>
     </>
   )
 }
