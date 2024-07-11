d67415f9ddf404c6cee6c916879a495020cf2000
De1t4 <mauriciochambicaceres@gmail.com>
2024-07-11 17:55:38 -0300

    add nav

--- frontend/src/pages/landing/Landing.tsx
+++ frontend/src/pages/landing/Landing.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { ArrowIcon, ArtistIcon, AvatarIcon, CloudIcon, FacebookIcon, FeaturesIcon, HamburguerIcon, LinkedinIcon, SecurityIcon, StarIcon, StorageIcon, TimeIcon, UpdateIcon, VideoIcon } from '../../components/icons/Icons'
+import { ArrowIcon, ArtistIcon, AvatarIcon, CloudIcon, FacebookIcon, FeaturesIcon, LinkedinIcon, SecurityIcon, StarIcon, StorageIcon, TimeIcon, UpdateIcon, VideoIcon } from '../../components/icons/Icons'
 import { Accordion, AccordionItem, Button, Input } from '@nextui-org/react'
+import styles from '../../styles.module.css';
+import { Nav } from '../../components/Nav';
 
 const items = [
   {
@@ -38,33 +39,28 @@ export const Landing = () => {
   const defaultContent = "Elementum ullamcorper felis nulla scelerisque. Nunc enim nunc mattis leo massa.";
   return (
     <main className=' border-2 border-gray w-96 m-auto max-md:w-full'>
-      <nav className=' flex justify-between p-4 border-b-2 border-gray-300 items-center'>
-        <img src={'./public/logo-justina.webp'} className=' w-40' alt="logo-justina" />
-        <span className=' cursor-pointer transition-all duration-300 hover:scale-105'>
-          <HamburguerIcon width={30} height={30}></HamburguerIcon>
-        </span>
-      </nav>
+      <Nav></Nav>
       <article className=' p-4 flex flex-col gap-y-10'>
         <section className=' flex flex-col gap-y-4'>
-          <h2 className=' font-bold text-2xl'>JUSTINA.IO</h2>
+          <h2 className={styles.h2}>JUSTINA.IO</h2>
           <p className=' text-lg text-gray-color'>At lacus vitae nulla sagittis scelerisque nisl. Pellentesque duis cursus vestibulum, facilisi ac, sed faucibus.</p>
           <Button color='primary' className=' rounded-sm w-36 h-11'>Get Started</Button>
           <img src={'./public/Image-generic.png'} alt="image-landing-page" />
         </section>
         <section className=' flex flex-col gap-y-8'>
-          <h5 className=' uppercase text-gray-color text-xl'>Acerca de</h5>
-          <h2 className='text-2xl font-semibold'>Etiam nulla lectus amet nunc molestie </h2>
+          <h5 className={styles.h5}>ACERCA DE</h5>
+          <h2 className={styles.h2}>Etiam nulla lectus amet nunc molestie </h2>
           <p className='text-xl text-gray-color leading-8 tracking-wide	'>Metus, diam pretium at at morbi vulputate et. Tellus ipsum sollicitudin ut eu a lectus potenti maecenas </p>
           <h4 className='text-xl flex items-center gap-x-3 text-blue-light-color font-semibold cursor-pointer hover:translate-x-2 transition-all duration-300'>Read More <ArrowIcon height={15} width={15}></ArrowIcon></h4>
           <img src={'./public/Image-generic.png'} alt="image-landing-page" />
         </section>
         <section className='flex flex-col gap-y-8 items-center text-center'>
-          <h5 className='uppercase text-gray-color text-xl'>Servicios</h5>
-          <h2 className='text-2xl font-semibold'>Etiam nulla lectus amet nunc molestie at vulputate.</h2>
-          <h5 className=' text-gray-color text-xl'>Neque, pulvinar vestibulum non aliquam.</h5>
+          <h5 className={styles.h5}>SERVICIOS</h5>
+          <h2 className={styles.h2}>Etiam nulla lectus amet nunc molestie at vulputate.</h2>
+          <h5 className={styles.h5}>Neque, pulvinar vestibulum non aliquam.</h5>
           <li className=' list-none flex flex-col gap-y-16'>
             {items.map((item, index) => (
-              <ol key={index} className=' flex flex-col justify-center items-center'>
+              <ol key={index} className=' gap-y-4 flex flex-col justify-center items-center'>
                 <span className="bg-light-color w-16 h-16 flex items-center justify-center rounded-full">
                   {item.icon}
                 </span>
@@ -75,28 +71,28 @@ export const Landing = () => {
         </section>
         <section className=' flex flex-col gap-y-8'>
           <div className=' text-center gap-y-4 flex flex-col'>
-            <h2 className='text-3xl font-semibold'>Diferenciadores</h2>
-            <h5 className='text-gray-color text-xl'>Neque, pulvinar vestibulum non aliquam.</h5>
+            <h2 className={styles.h2}>Diferenciadores</h2>
+            <h5 className={styles.h5}>Neque, pulvinar vestibulum non aliquam.</h5>
           </div>
           <div className="flex flex-col gap-4">
             <h4 className='text-2xl font-semibold'>Purus id tellus arcu habitant proin magna. Integer purus </h4>
-            <h5 className='text-gray-color text-xl'>Facilisi viverra dictum augue eu lobortis elit. In et donec habitasse lacus mi commodo elementum.</h5>
+            <h5 className={styles.h5}>Facilisi viverra dictum augue eu lobortis elit. In et donec habitasse lacus mi commodo elementum.</h5>
             <img src={'./public/Image-generic.png'} alt="image-landing-page" />
           </div>
           <div className="flex flex-col gap-4">
             <h4 className='text-2xl font-semibold'>Purus id tellus arcu habitant proin magna. Integer purus </h4>
-            <h5 className='text-gray-color text-xl'>Facilisi viverra dictum augue eu lobortis elit. In et donec habitasse lacus mi commodo elementum.</h5>
+            <h5 className={styles.h5}>Facilisi viverra dictum augue eu lobortis elit. In et donec habitasse lacus mi commodo elementum.</h5>
             <img src={'./public/Image-generic.png'} alt="image-landing-page" />
           </div>
           <div className="flex flex-col gap-4">
             <h4 className='text-2xl font-semibold'>Purus id tellus arcu habitant proin magna. Integer purus </h4>
-            <h5 className='text-gray-color text-xl'>Facilisi viverra dictum augue eu lobortis elit. In et donec habitasse lacus mi commodo elementum.</h5>
+            <h5 className={styles.h5}>Facilisi viverra dictum augue eu lobortis elit. In et donec habitasse lacus mi commodo elementum.</h5>
             <img src={'./public/Image-generic.png'} alt="image-landing-page" />
           </div>
         </section>
         <section className='flex flex-col gap-y-6'>
-          <h2 className='text-2xl font-semibold'>Preguntas Frecuentes</h2>
-          <h5 className='text-gray-color text-xl'>Pellentesque cras adipiscing tempus libero vel nullam mauris tellus. Aliquam ultrices tellus consequat amet, lectus aliquam est in neque.</h5>
+          <h2 className={styles.h2}>Preguntas Frecuentes</h2>
+          <h5 className={styles.h5}>Pellentesque cras adipiscing tempus libero vel nullam mauris tellus. Aliquam ultrices tellus consequat amet, lectus aliquam est in neque.</h5>
           <Accordion className=' gap-y-4 flex flex-col'>
             <AccordionItem key="1" aria-label="How soon will my order ship?" title="How soon will my order ship?">
               {defaultContent}
@@ -117,8 +113,8 @@ export const Landing = () => {
 
         </section>
         <section className='text-center flex items-center flex-col gap-4'>
-          <h2 className='text-3xl font-semibold '>Clientes</h2>
-          <h5 className='text-gray-color text-xl'>Neque, pulvinar vestibulum non aliquam.</h5>
+          <h2 className={styles.h2}>Clientes</h2>
+          <h5 className={styles.h5}>Neque, pulvinar vestibulum non aliquam.</h5>
           <div className="flex  gap-2">
             <StarIcon width={30} height={30}></StarIcon>
             <StarIcon width={30} height={30}></StarIcon>
@@ -145,7 +141,7 @@ export const Landing = () => {
       <footer className='flex flex-col  gap-y-8 p-4'>
         <div>
           <h6 className=' tracking-wider mb-2 font-bold'>Product</h6>
-          <li className=' flex flex-col list-none text-gray-color'>
+          <li className=' flex flex-col list-none text-gray-color leading-8 font-normal'>
             <ol>Mi feugiat</ol>
             <ol>Netus fermentum</ol>
             <ol>Id dolor</ol>
@@ -155,7 +151,7 @@ export const Landing = () => {
         </div>
         <div>
           <h6 className=' tracking-wider mb-2 font-bold'>Information</h6>
-          <li className=' flex flex-col list-none text-gray-color'>
+          <li className=' flex flex-col list-none text-gray-color leading-8 font-normal'>
             <ol>Mi feugiat</ol>
             <ol>Netus fermentum</ol>
             <ol>Id dolor</ol>
@@ -165,7 +161,7 @@ export const Landing = () => {
         </div>
         <div>
           <h6 className=' tracking-wider mb-2 font-bold'>Company</h6>
-          <li className=' flex flex-col list-none text-gray-color'>
+          <li className=' flex flex-col list-none text-gray-color leading-8 font-normal'>
             <ol>Mi feugiat</ol>
             <ol>Netus fermentum</ol>
             <ol>Id dolor</ol>
