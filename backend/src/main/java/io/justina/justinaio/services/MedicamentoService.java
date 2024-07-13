63c86147115be5837dd8d79337e79bb25548ef86
Ivan Alexandro Moroz <AlexandroMoroz5@gmail.com>
2024-07-12 22:25:59 -0300

    feat: :sparkles: CRUD de medicamento
    Se creó el CRUD completo de medicamentos.

---
+++ backend/src/main/java/io/justina/justinaio/services/MedicamentoService.java
@@ -0,0 +1,100 @@
+package io.justina.justinaio.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import io.justina.justinaio.dto.BajaPorNombreRequest;
+import io.justina.justinaio.dto.MedicamentoModificacionRequest;
+import io.justina.justinaio.dto.MedicamentoRequest;
+import io.justina.justinaio.dto.MedicamentoResponse;
+import io.justina.justinaio.model.Medicamento;
+import io.justina.justinaio.repositories.MedicamentoRepository;
+import io.justina.justinaio.util.Mapper;
+import io.justina.justinaio.util.PageResponse;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class MedicamentoService {
+
+    private final MedicamentoRepository medicamentoRepository;
+
+    public void crearMedicamento(MedicamentoRequest medicamentoRequest) {
+
+        if (medicamentoRequest.getNombre() == null || medicamentoRequest.getNombre().isEmpty()) {
+            throw new IllegalArgumentException("Medicamento sin nombre");
+        }
+
+        if (medicamentoRequest.getDescripcion() == null || medicamentoRequest.getDescripcion().isEmpty()) {
+            throw new IllegalArgumentException("Medicamento sin descripción");
+        }
+
+        Optional<Medicamento> existente = medicamentoRepository.buscarPorNombre(medicamentoRequest.getNombre());
+        if (existente.isPresent()) {
+            throw new IllegalArgumentException("El medicamento ya existe");
+        }
+
+        medicamentoRepository.save(Mapper.toMedicamento(medicamentoRequest));
+    }
+
+    public void modificarMedicamento(MedicamentoModificacionRequest medicamentoRequest) {
+        Medicamento medicamento = medicamentoRepository.buscarPorNombre(medicamentoRequest.getNombre()).orElseThrow(
+                () -> new NullPointerException("Medicamento a modificar no encontrado"));
+
+        if (medicamentoRequest.getNombreCambio() != null) {
+            medicamento.setNombre(medicamentoRequest.getNombreCambio());
+        }
+
+        if (medicamentoRequest.getDescripcion() != null) {
+            medicamento.setDescripcion(medicamentoRequest.getDescripcion());
+        }
+
+        medicamentoRepository.save(medicamento);
+    }
+
+    public void bajaMedicamento(BajaPorNombreRequest bajaMedicamentoRequest) {
+
+        Medicamento medicamento = medicamentoRepository.buscarPorNombre(bajaMedicamentoRequest.getNombre())
+                .orElseThrow(
+                        () -> new NullPointerException("Medicamento a dar de baja no encontrado"));
+        medicamento.setEsActivo(false);
+        medicamentoRepository.save(medicamento);
+
+    }
+
+    public PageResponse<MedicamentoResponse> buscarMedicamentos(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("nombre").ascending());
+        Page<Medicamento> medicamentos = medicamentoRepository.findByEsActivoTrue(pageable);
+        List<MedicamentoResponse> medicamentosResponse = medicamentos.stream()
+                .map(Mapper::toMedicamentoResponse).toList();
+        return new PageResponse<>(
+                medicamentosResponse,
+                medicamentos.getNumber(),
+                medicamentos.getSize(),
+                medicamentos.getTotalElements(),
+                medicamentos.getTotalPages(),
+                medicamentos.isFirst(),
+                medicamentos.isLast());
+    }
+
+    public Object buscarUnMedicamento(String nombre) {
+
+        Optional<Medicamento> medicamento = medicamentoRepository.buscarMedicamento(nombre);
+
+        if (medicamento.isEmpty()) {
+            throw new NullPointerException("Medicamento no encontrado");
+        }
+
+        MedicamentoResponse medicamentoResponse = new MedicamentoResponse();
+        medicamentoResponse.setNombre(medicamento.get().getNombre());
+        medicamentoResponse.setDescripcion(medicamento.get().getDescripcion());
+        return medicamentoResponse;
+    }
+
+}
--- backend/src/main/java/io/justina/justinaio/util/Mapper.java
+++ backend/src/main/java/io/justina/justinaio/util/Mapper.java
@@ -8,61 +8,80 @@ import java.util.stream.Collectors;
 
 public class Mapper {
 
-    public static Medico toMedico(MedicoRequest medicoRequest, Especialidad especialidad,
-            List<Financiador> financiadores) {
-        return Medico.builder()
-                .nombre(medicoRequest.getNombre())
-                .apellido(medicoRequest.getApellido())
-                .telefono(medicoRequest.getTelefono())
-                .provincia(medicoRequest.getProvincia())
-                .localidad(medicoRequest.getLocalidad())
-                .licencia(medicoRequest.getLicencia())
-                .especialidad(especialidad)
-                .financiadores(financiadores)
-                .build();
-    }
+        public static Medico toMedico(MedicoRequest medicoRequest, Especialidad especialidad,
+                        List<Financiador> financiadores) {
+                return Medico.builder()
+                                .nombre(medicoRequest.getNombre())
+                                .apellido(medicoRequest.getApellido())
+                                .telefono(medicoRequest.getTelefono())
+                                .provincia(medicoRequest.getProvincia())
+                                .localidad(medicoRequest.getLocalidad())
+                                .licencia(medicoRequest.getLicencia())
+                                .especialidad(especialidad)
+                                .financiadores(financiadores)
+                                .build();
+        }
 
-    public static MedicoResponse toMedicoResponse(Medico medico) {
-        return MedicoResponse.builder()
-                .idMedico(medico.getIdMedico())
-                .nombre(medico.getNombre())
-                .apellido(medico.getApellido())
-                .telefono(medico.getTelefono())
-                .provincia(medico.getProvincia())
-                .localidad(medico.getLocalidad())
-                .licencia(medico.getLicencia())
-                .especialidad(medico.getEspecialidad().getNombre())
-                .build();
-    }
+        public static MedicoResponse toMedicoResponse(Medico medico) {
+                return MedicoResponse.builder()
+                                .idMedico(medico.getIdMedico())
+                                .nombre(medico.getNombre())
+                                .apellido(medico.getApellido())
+                                .telefono(medico.getTelefono())
+                                .provincia(medico.getProvincia())
+                                .localidad(medico.getLocalidad())
+                                .licencia(medico.getLicencia())
+                                .especialidad(medico.getEspecialidad().getNombre())
+                                .build();
+        }
 
-    public static Financiador toFinanciador(FinanciadorRequest financiadorRequest) {
-        return Financiador.builder()
-                .nombre(financiadorRequest.getNombre())
-                .descripcion(financiadorRequest.getDescripcion())
-                .esActivo(true)
-                .build();
-    }
+        public static Financiador toFinanciador(FinanciadorRequest financiadorRequest) {
+                return Financiador.builder()
+                                .nombre(financiadorRequest.getNombre())
+                                .descripcion(financiadorRequest.getDescripcion())
+                                .esActivo(true)
+                                .build();
+        }
 
-    public static PacienteResponse toPacienteResponse(Paciente paciente) {
-        return PacienteResponse.builder()
-                .idPaciente(paciente.getIdPaciente())
-                .nombre(paciente.getNombre())
-                .apellido(paciente.getApellido())
-                .tipoDocumento(paciente.getTipoDocumento().getDescripcion())
-                .numeroDocumento(paciente.getNumeroDocumento())
-                .patologia(paciente.getPatologia().getNombre())
-                .financiador(paciente.getFinanciador().getNombre())
-                .tratamientos(
-                        paciente.getTratamientos().stream().map(Tratamiento::getNombre).collect(Collectors.toList()))
-                .medicos(paciente.getMedicos().stream().map(Medico::getFullName).collect(Collectors.toList()))
-                .entidades(paciente.getEntidades().stream().map(Entidad::getNombre).collect(Collectors.toList()))
-                .build();
-    }
+        public static FinanciadorResponse toFinanciadorResponse(Financiador financiador) {
+                return FinanciadorResponse.builder()
+                                .nombre(financiador.getNombre())
+                                .descripcion(financiador.getDescripcion())
+                                .build();
+        }
 
-    public static FinanciadorResponse toFinanciadorResponse(Financiador financiador) {
-        return FinanciadorResponse.builder()
-                .nombre(financiador.getNombre())
-                .descripcion(financiador.getDescripcion())
-                .build();
-    }
+        public static Medicamento toMedicamento(MedicamentoRequest medicamentoRequest) {
+                return Medicamento.builder()
+                                .nombre(medicamentoRequest.getNombre())
+                                .descripcion(medicamentoRequest.getDescripcion())
+                                .esActivo(true)
+                                .build();
+        }
+
+        public static MedicamentoResponse toMedicamentoResponse(Medicamento medicamento) {
+                return MedicamentoResponse.builder()
+                                .nombre(medicamento.getNombre())
+                                .descripcion(medicamento.getDescripcion())
+                                .build();
+        }
+
+        public static PacienteResponse toPacienteResponse(Paciente paciente) {
+                return PacienteResponse.builder()
+                                .idPaciente(paciente.getIdPaciente())
+                                .nombre(paciente.getNombre())
+                                .apellido(paciente.getApellido())
+                                .tipoDocumento(paciente.getTipoDocumento().getDescripcion())
+                                .numeroDocumento(paciente.getNumeroDocumento())
+                                .patologia(paciente.getPatologia().getNombre())
+                                .financiador(paciente.getFinanciador().getNombre())
+                                .tratamientos(
+                                                paciente.getTratamientos().stream().map(Tratamiento::getNombre)
+                                                                .collect(Collectors.toList()))
+                                .medicos(paciente.getMedicos().stream().map(Medico::getFullName)
+                                                .collect(Collectors.toList()))
+                                .entidades(paciente.getEntidades().stream().map(Entidad::getNombre)
+                                                .collect(Collectors.toList()))
+                                .build();
+        }
+
 }
