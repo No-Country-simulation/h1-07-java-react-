72f16bfaf52c442d4aa556e6182338e650ab28a3
De1t4 <mauriciochambicaceres@gmail.com>
2024-07-16 21:33:13 -0300

    delete folder assets and services

--- frontend/src/output/models/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './AuthenticationRequest';
-export * from './AuthenticationResponse';
-export * from './MedicoRequest';
-export * from './PacienteRequest';
-export * from './PasswordRequest';
-export * from './RegistrationRequest';
-export * from './RolRequest';
