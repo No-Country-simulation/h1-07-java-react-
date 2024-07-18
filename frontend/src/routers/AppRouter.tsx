91da6f4ad1672928284ecd66d32045c4a0a431a4
Facundo Pettersson <petterssonfacundo@gmail.com>
2024-07-18 18:23:44 -0300

    Add: HomePage And UserInfo Pages

--- frontend/src/routers/AppRouter.tsx
+++ frontend/src/routers/AppRouter.tsx
@@ -4,12 +4,14 @@ import { Toaster } from "sonner";
 import { ErrorPage } from "../pages/NotFound/ErrorPage";
 import { LoginPage } from "../pages/Login/Login";
 import { Onboarding } from "../pages/OnBoarding/OnBoarding";
-import SignUp from "../pages/SignUp/SignUp";
-import { Landing } from "../pages/Landing/Landing";
-import { ActiveAccount } from "../pages/Active-account/ActiveAccount";
+import SignUp from "../pages/signup/SignUp";
+import { Landing } from "../pages/landing/Landing";
+import { ActiveAccount } from "../pages/active-account/ActiveAccount";
 import { AuthContextProvider } from "../Context/AuthContext";
-import { Dashboard } from "../pages/private/Dashboard/Dashboard";
 import { RoutesWithNotFound } from "./RoutesWithNotFound";
+import { Home } from "../pages/private/Dashboard/Home/Home";
+import { Detalle } from "../pages/private/Dashboard/Detalle/Detalle";
+import { UserInfo } from "../pages/private/Dashboard/UserInfo/UserInfo";
 function AppRouter() {
 
 	return (
@@ -17,14 +19,15 @@ function AppRouter() {
 			<AuthContextProvider>
 				<BrowserRouter>
 					<RoutesWithNotFound>
-						<Route path="/" element={<p>hola</p>} />
 						<Route path="/login" element={<LoginPage />} />
 						<Route path="/signup" element={<SignUp />} />
 						<Route path="/onboarding" element={<Onboarding />} />
-						<Route path="/landing" element={<Landing />}></Route>
+						<Route path="/" element={<Landing />}></Route>
 						<Route path="/active-account" element={<ActiveAccount />}></Route>
 						<Route element={<PrivateRoute />}>
-							<Route path="/dashboard" index element={<Dashboard />} />
+							<Route path="/dashboard" index element={<Home />} />
+							<Route path="/detalle" element={<Detalle />}/>
+							<Route path="/userInfo" element={<UserInfo />}/>
 						</Route>
 						<Route path="*" element={<ErrorPage />} />
 					</RoutesWithNotFound>
