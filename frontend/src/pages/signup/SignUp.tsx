d4c9f4a13fbcaaedfd3eebfc254667ff7bfa18c8
Facundo Pettersson <petterssonfacundo@gmail.com>
2024-07-23 22:31:00 -0300

    Add: ValidationRouteRol

--- frontend/src/pages/signup/SignUp.tsx
+++ frontend/src/pages/signup/SignUp.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { toast } from 'sonner';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import { Button } from '@nextui-org/react';
-import { CardIcon, FlechaIconTwo, JobIcon, LoaderIcon } from '../../components/icons/Icons';
+import { CardIcon, FlechaIconTwo, JobIcon, LoaderIcon } from '../../../public/icons/Icons';
 import { useAuthContext } from '../../Context/AuthContext';
 import { dataRegisterDoctor, initialValuesDoctor } from '../../utils/data/data';
 import { validationSchema } from '../../utils/validation/validation';
