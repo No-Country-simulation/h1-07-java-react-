f6f7c41aa68fc8945203fc3cd38be1f780307231
De1t4 <mauriciochambicaceres@gmail.com>
2024-07-26 19:50:18 -0300

    add chatbot

--- frontend/src/pages/DeviceDetection/DeviceDetection.tsx
+++ frontend/src/pages/DeviceDetection/DeviceDetection.tsx
@@ -1,35 +1,45 @@
 import React, { useEffect, useState } from 'react';
+import AppRouter from '../../routers/AppRouter';
 
-interface PropsDeviceDetection {
-  children?: React.ReactNode;
-}
 
-const DeviceDetection: React.FC<PropsDeviceDetection> = ({ children }) => {
+
+const DeviceDetection: React.FC = () => {
   const [isMobile, setIsMobile] = useState<boolean>(false);
+  const [isAndroidOrIOS, setIsAndroidOrIOS] = useState<boolean>(false);
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 568);
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    const checkDevice = () => {
+      const userAgent = navigator.userAgent || navigator.vendor;
+      if (/android/i.test(userAgent) || /iPad|iPhone|iPod/.test(userAgent)) {
+        setIsAndroidOrIOS(true);
+      } else {
+        setIsAndroidOrIOS(false);
+      }
     };
 
     handleResize();
+    checkDevice();
+
     window.addEventListener('resize', handleResize);
 
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
-  const isAndroidOrIOS = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 
   return (
     <>
       {isMobile && isAndroidOrIOS ? (
-        children
+        <AppRouter />
       ) : (
-        <div className="flex flex-col justify-center items-center h-screen bg-black text-white font-inter">
-          <h1>Esta aplicación solo está disponible para dispositivos móviles con Android o iOS.</h1>
-          <p>Por favor, escanee el código QR</p>
-          <img src="QR/qrcode-generado.png" alt="QR Code" />
+        <div className="flex flex-col justify-center items-center h-screen  font-inter">
+          <h1 className='text-center'>Esta aplicación solo está disponible para dispositivos móviles con Android o iOS.</h1>
+          <p>Por favor, Scanner el código QR</p>
+          <img src="QR/qrcode-generado.png" alt="" />
         </div>
       )}
     </>
