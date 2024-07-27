a517ccc4672a2aa1215d3036a354b6278b423027
PabloMagna <pablo.magnavachi@alumnos.frgp.utn.edu.ar>
2024-07-27 12:42:20 -0300

    fix: busqueda dontante mejorada - localhost en properties

--- backend/src/main/java/io/justina/justinaio/controller/DonanteController.java
+++ backend/src/main/java/io/justina/justinaio/controller/DonanteController.java
@@ -6,6 +6,9 @@ import io.justina.justinaio.dto.DonanteResponse;
 import io.justina.justinaio.dto.MedicoDonanteRequest;
 import io.justina.justinaio.services.DonanteService;
 import io.justina.justinaio.util.PageResponse;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -28,7 +31,7 @@ public class DonanteController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/buscar-donantes")
+    /*@GetMapping("/buscar-donantes")
     public ResponseEntity<PageResponse<DonanteResponse>> buscarDonantes(
             Authentication token,
             @RequestParam(name = "textoBusqueda", required = false) String textoBusqueda,
@@ -45,7 +48,88 @@ public class DonanteController {
                 .edad(edad)
                 .build();
         return ResponseEntity.ok(donanteService.buscarDonantes(page, size, busqueda, token));
+    }*/
+
+    @GetMapping("/buscar-donantes")
+    public ResponseEntity<PageResponse<DonanteResponse>> buscarDonantes(
+            Authentication token,
+            @Parameter(
+                    in = ParameterIn.QUERY,
+                    description = "Texto descripción donacion",
+                    example = "higado"
+            )
+            @RequestParam(name = "textoBusqueda", required = false) String textoBusqueda,
+            @Parameter(
+                    in = ParameterIn.QUERY,
+                    description = "Género del donante (0: masculino, 1: femenino, 2: otro)"
+            )
+            @RequestParam(name = "generoOrdinal", required = false) Integer generoOrdinal,
+            @Parameter(
+                    in = ParameterIn.QUERY,
+                    description = "Factor sanguíneo del donante (0: O+, 1: O-, 2: A+, 3: A-, 4: B+, 5: B-, 6: AB+, 7: AB-)"
+            )
+            @RequestParam(name = "factorSanguineoOrdinal", required = false) Integer factorSanguineoOrdinal,
+            @Parameter(
+                    in = ParameterIn.QUERY,
+                    description = "Edad del donante en años",
+                    schema = @Schema(minimum = "0")
+            )
+            @RequestParam(name = "edad", required = false) Integer edad,
+            @Parameter(
+                    in = ParameterIn.QUERY,
+                    description = "Filtro de edad (mayor, menor, igual)"
+            )
+            @RequestParam(name = "edadFiltro", required = false) String edadFiltro,
+            @Parameter(
+                    in = ParameterIn.QUERY,
+                    description = "Peso del donante en kilogramos",
+                    schema = @Schema(minimum = "0")
+            )
+            @RequestParam(name = "peso", required = false) Integer peso,
+            @Parameter(
+                    in = ParameterIn.QUERY,
+                    description = "Filtro de peso (mayor, menor, igual)"
+            )
+            @RequestParam(name = "pesoFiltro", required = false) String pesoFiltro,
+            @Parameter(
+                    in = ParameterIn.QUERY,
+                    description = "Altura del donante en centímetros",
+                    schema = @Schema(minimum = "0")
+            )
+            @RequestParam(name = "altura", required = false) Integer altura,
+            @Parameter(
+                    in = ParameterIn.QUERY,
+                    description = "Filtro de altura (mayor, menor, igual)"
+            )
+            @RequestParam(name = "alturaFiltro", required = false) String alturaFiltro,
+            @Parameter(
+                    in = ParameterIn.QUERY,
+                    description = "Número de página para la paginación",
+                    schema = @Schema(defaultValue = "0")
+            )
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @Parameter(
+                    in = ParameterIn.QUERY,
+                    description = "Tamaño de página para la paginación",
+                    schema = @Schema(defaultValue = "10")
+            )
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size
+    ) {
+        BusquedaDonanteRequest busqueda = BusquedaDonanteRequest.builder()
+                .texto(textoBusqueda)
+                .generoOrdinal(generoOrdinal)
+                .factorSanguineoOrdinal(factorSanguineoOrdinal)
+                .edad(edad)
+                .edadFiltro(edadFiltro)
+                .peso(peso)
+                .pesoFiltro(pesoFiltro)
+                .altura(altura)
+                .alturaFiltro(alturaFiltro)
+                .build();
+        return ResponseEntity.ok(donanteService.buscarDonantes(page, size, busqueda, token));
     }
+
+
 
     @GetMapping("/buscar-medico-por-donante")
     public ResponseEntity<MedicoDonanteRequest> buscarDonante(
--- backend/src/main/java/io/justina/justinaio/util/DonanteSpecification.java
+++ backend/src/main/java/io/justina/justinaio/util/DonanteSpecification.java
@@ -2,50 +2,70 @@ package io.justina.justinaio.util;
 
 import io.justina.justinaio.dto.BusquedaDonanteRequest;
 import io.justina.justinaio.model.Donante;
-import io.justina.justinaio.model.enums.FactorSanguineo;
-import io.justina.justinaio.model.enums.Genero;
 import org.springframework.data.jpa.domain.Specification;
 
-import java.time.LocalDate;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Path;
 import jakarta.persistence.criteria.Predicate;
+import java.time.LocalDate;
 
 public class DonanteSpecification {
 
-    public static Specification<Donante> containsTextInAttributes(BusquedaDonanteRequest busquedaRequest) {
-        return (root, query, criteriaBuilder) -> {
-            Predicate combinedPredicate = criteriaBuilder.conjunction();
+    public static Specification<Donante> buildSpecification(BusquedaDonanteRequest request) {
+        return (root, query, cb) -> {
+            Predicate p = cb.conjunction();
 
-            if (busquedaRequest.getTexto() != null && !busquedaRequest.getTexto().isEmpty()) {
-                String pattern = "%" + busquedaRequest.getTexto().toLowerCase() + "%";
-                Predicate textPredicate = criteriaBuilder.or(
-                        criteriaBuilder.like(criteriaBuilder.lower(root.get("nombre")), pattern),
-                        criteriaBuilder.like(criteriaBuilder.lower(root.get("apellido")), pattern),
-                        criteriaBuilder.like(criteriaBuilder.lower(root.get("altura").as(String.class)), pattern),
-                        criteriaBuilder.like(criteriaBuilder.lower(root.get("peso").as(String.class)), pattern)
-                );
-                combinedPredicate = criteriaBuilder.and(combinedPredicate, textPredicate);
+            if (request.getTexto() != null && !request.getTexto().isEmpty()) {
+                p = cb.and(p, cb.like(cb.lower(root.get("descripcion")), "%" + request.getTexto().toLowerCase() + "%"));
+            }
+            if (request.getGeneroOrdinal() != null) {
+                p = cb.and(p, cb.equal(root.get("genero"), request.getGeneroOrdinal()));
+            }
+            if (request.getFactorSanguineoOrdinal() != null) {
+                p = cb.and(p, cb.equal(root.get("factorSanguineo"), request.getFactorSanguineoOrdinal()));
+            }
+            if (request.getEdad() != null && request.getEdadFiltro() != null) {
+                LocalDate birthDate = calculateBirthDateFromAge(request.getEdad());
+                p = addDateComparisonPredicate(p, cb, root.get("fechaNacimiento"), birthDate, request.getEdadFiltro());
+            }
+            if (request.getPeso() != null && request.getPesoFiltro() != null) {
+                p = addComparisonPredicate(p, cb, root.get("peso"), request.getPeso(), request.getPesoFiltro());
+            }
+            if (request.getAltura() != null && request.getAlturaFiltro() != null) {
+                p = addComparisonPredicate(p, cb, root.get("altura"), request.getAltura(), request.getAlturaFiltro());
             }
 
-            if (busquedaRequest.getGeneroOrdinal() != null) {
-                Predicate genderPredicate = criteriaBuilder.equal(root.get("genero"), Genero.values()[busquedaRequest.getGeneroOrdinal()]);
-                combinedPredicate = criteriaBuilder.and(combinedPredicate, genderPredicate);
-            }
-
-            if (busquedaRequest.getFactorSanguineoOrdinal() != null) {
-                Predicate factorSanguineoPredicate = criteriaBuilder.equal(root.get("factorSanguineo"),
-                        FactorSanguineo.values()[busquedaRequest.getFactorSanguineoOrdinal()]);
-                combinedPredicate = criteriaBuilder.and(combinedPredicate, factorSanguineoPredicate);
-            }
-
-            if (busquedaRequest.getEdad() != null) {
-                LocalDate currentDate = LocalDate.now();
-                LocalDate birthDateFrom = currentDate.minusYears(busquedaRequest.getEdad() + 1).plusDays(1);
-                LocalDate birthDateTo = currentDate.minusYears(busquedaRequest.getEdad());
-                Predicate agePredicate = criteriaBuilder.between(root.get("fechaNacimiento"), birthDateFrom, birthDateTo);
-                combinedPredicate = criteriaBuilder.and(combinedPredicate, agePredicate);
-            }
-
-            return combinedPredicate;
+            return p;
         };
+    }
+
+    private static Predicate addComparisonPredicate(Predicate p, CriteriaBuilder cb, Path<Integer> path, Integer value, String filter) {
+        switch (filter.toLowerCase()) {
+            case "mayor":
+                return cb.and(p, cb.greaterThan(path, value));
+            case "menor":
+                return cb.and(p, cb.lessThan(path, value));
+            case "igual":
+                return cb.and(p, cb.equal(path, value));
+            default:
+                throw new IllegalArgumentException("Filtro no válido: " + filter);
+        }
+    }
+
+    private static Predicate addDateComparisonPredicate(Predicate p, CriteriaBuilder cb, Path<LocalDate> path, LocalDate date, String filter) {
+        switch (filter.toLowerCase()) {
+            case "mayor":
+                return cb.and(p, cb.lessThan(path, date));
+            case "menor":
+                return cb.and(p, cb.greaterThan(path, date));
+            case "igual":
+                return cb.and(p, cb.equal(path, date));
+            default:
+                throw new IllegalArgumentException("Filtro no válido: " + filter);
+        }
+    }
+
+    private static LocalDate calculateBirthDateFromAge(Integer age) {
+        return LocalDate.now().minusYears(age);
     }
 }
