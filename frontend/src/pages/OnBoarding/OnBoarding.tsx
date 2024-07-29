7d7723aac7bc907a2cc86821595f573a7028c7cf
Facundo Pettersson <petterssonfacundo@gmail.com>
2024-07-29 15:57:33 -0300

    Add: AddMejoras

--- frontend/src/pages/OnBoarding/OnBoarding.tsx
+++ frontend/src/pages/OnBoarding/OnBoarding.tsx
@@ -5,7 +5,7 @@ export function Onboarding() {
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        // Simula la carga durante 3 segundos
+
         const timer = setTimeout(() => {
             setIsLoading(false);
         }, 2000);
@@ -17,7 +17,7 @@ export function Onboarding() {
         return (
             <div className="flex justify-center items-center h-full ">
                 <div className=' blur-sm'>
-                    <img src="IMG_WEBP/imagen-2.webp" alt="" className="w-[100vh] h-[100vh] relative" />
+                    <img src="imagen-2.png" alt="" className="w-[100vh] h-[100vh] relative" />
                 </div>
                 <div className=" flex flex-col items-center pt-5 w-[100%] absolute top-[14rem] rounded-t-[3rem] mb-2">
                     <img src="JustinaIO_logo_page-02.png" alt="" />
@@ -33,23 +33,20 @@ export function Onboarding() {
     }
 
     return (
-        <div className="flex justify-center items-center h-full ">
-            <div>
-                <img src="IMG_WEBP/Imagen-1.webp" alt="" className="w-[100vh] h-full relative" />
-            </div>
-            <div className="bg-[#F9F9F9] flex flex-col items-center pt-5 w-[100%] h-[45%] absolute top-[27rem] border rounded-t-[3rem] mb-2">
-                <h1 className="text-center font-bold font-inter text-[22px] w-[80%] text-[#1A1A1A]">Comprometidos con la salud y bienestar de todos</h1>
-                <p className="text-[#948ABC] text-center mt-3 w-[55%] text-[12px]">Conectamos médicos y pacientes para facilitar el acceso a la salud</p>
+        <div className="flex flex-col justify-between items-center min-h-screen bg-white">
+            <img src="IMG_WEBP/Imagen-1.webp" alt="" className="w-screen h-auto mb-4" />
+            <div className="absolute top-[30rem] bg-[#F9F9F9]  w-full flex flex-col items-center pt-5 border-t-2 rounded-t-[2.5rem] mt-auto ">
+                <h1 className="text-center font-bold font-inter text-[20px] w-4/5 text-[#1A1A1A]">Comprometidos con la salud y bienestar de todos</h1>
+                <p className="text-[#948ABC] font-inter text-center mt-3 w-3/5 text-[12px]">Conectamos médicos y pacientes para facilitar el acceso a la salud</p>
                 <Link to={"/signup"}>
-                    <button className="mt-6 mb-4 bg-[#E08733] p-1 rounded-[20px] text-white font-inter px-[70px]">
+                    <button className="mt-6 mb-4 bg-[#E08733] py-2 px-10 rounded-full text-white font-inter">
                         Crear cuenta
                     </button>
                 </Link>
-                <p className="pb-3 text-[18px] text-primary-brand-dark font-bold font-inter">ó</p>
+                <p className="pb-1 text-[18px] text-primary-brand-dark font-bold font-inter">ó</p>
                 <Link to={"/login"}>
                     <p className="text-primary-brand-dark font-bold font-inter">Iniciar sesión</p>
                 </Link>
-                <span className="bg-black p-[1px] mt-[15px] w-[150px]"></span>
             </div>
         </div>
     );
