f6ffb159046fed55a640850fe49b1c1eb268e8f8
De1t4 <mauriciochambicaceres@gmail.com>
2024-07-29 15:44:44 -0300

    add page donations chat-cora and new component menu aside

--- frontend/src/utils/functions/functions.ts
+++ frontend/src/utils/functions/functions.ts
@@ -28,3 +28,14 @@ export const getRandomColor = () => {
   ];
   return colors[Math.floor(Math.random() * colors.length)];
 };
+
+export function getAge(dateString:string) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+  }
+  return age;
+}
