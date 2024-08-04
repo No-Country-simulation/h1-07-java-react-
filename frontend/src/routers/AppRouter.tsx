92c31fc1f266cee3b8d04d93852079dd0b104f49
Agustin-desktop <agustin2051@gmail.com>
2024-08-04 17:26:26 -0300

    fix:mauriciobranch

--- frontend/src/routers/AppRouter.tsx
+++ frontend/src/routers/AppRouter.tsx
@@ -30,8 +30,8 @@ import Adherence from "../pages/private/Medic_Pages/Adherence/Adherence";
 import TreatmentAdherence from "../pages/private/Medic_Pages/Adherence/TreatmentAdherence/TreatmentAdherence";
 import { TreatmentPatient } from "../pages/private/Medic_Pages/Treatment/Treatment-patient";
 import Donations from "../pages/private/Medic_Pages/Donations/Donation_Home/Donations";
-import { ActiveAccount } from "../pages/Active-account/ActiveAccount";
-import SignUp from "../pages/SignUp/SignUp";
+import SignUp from "../pages/signup/SignUp";
+import { ActiveAccount } from "../pages/active-account/ActiveAccount";
 
 function AppRouter() {
   return (
