3d21881aed281ba039efc8ff1100a6610636c94c
De1t4 <mauriciochambicaceres@gmail.com>
2024-08-04 23:13:35 -0300

    add function search medic

--- frontend/src/pages/private/Medic_Pages/Donations/Donation_Home/Donations.tsx
+++ frontend/src/pages/private/Medic_Pages/Donations/Donation_Home/Donations.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 // import { AsideMenu } from "../../../../../components/AsideMenu";
-import { getAge } from "../../../../../utils/functions/functions";
 // import { Header_Donation } from "../../../../../components/Header_Medic_Donation/Header_Donation";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { Header_Donation } from "../../../../../components/Header_Medic_Donation/Header_Donation";
 import { API_URL } from "../../../../../api/api";
 import { ContentDonations } from "../../../../../Interfaces/interfaces";
+import { initialValuesFilter } from "../../../../../utils/data/data";
+import { LoaderIcon, SilderIcon } from "../../../../../../public/icons/Icons";
+import { Donors } from "./Donors/Donors";
 
 export interface Donation {
   textoBusqueda: string
@@ -20,17 +22,6 @@ export interface Donation {
   alturaFiltro: "mayor" | "menor" | "igual" | ""
 }
 
-const initialValuesFilter: Donation = {
-  textoBusqueda: "",
-  edad: "",
-  peso: "",
-  altura: "",
-  generoOrdinal: "",
-  factorSanguineoOrdinal: "",
-  edadFiltro: "",
-  pesoFiltro: "",
-  alturaFiltro: ""
-}
 
 const validationSchema = Yup.object({
   textoBusqueda: Yup.string(),
@@ -45,6 +36,8 @@ const validationSchema = Yup.object({
 
 export default function Donations() {
   const [donors, setDonors] = useState<ContentDonations>();
+  const [isOpenFilter, setIsOpenFilter] = useState(true)
+  const [loading, setLoading] = useState(false)
   const buildUrl = (values: Donation) => {
     const params = new URLSearchParams();
 
@@ -72,10 +65,10 @@ export default function Donations() {
 
   const handleSubmitFilterDonation = async (values: any) => {
     const token = localStorage.getItem('TOKEN_KEY');
-    console.log(values)
     const url = buildUrl(values);
 
     try {
+      setLoading(true)
       const res = await fetch(url, {
         method: "GET",
         headers: {
@@ -86,9 +79,10 @@ export default function Donations() {
 
       const data = await res.json();
       setDonors(data);
-      console.log(data);
     } catch (err) {
       console.log(err);
+    } finally {
+      setLoading(false)
     }
   };
 
@@ -98,113 +92,122 @@ export default function Donations() {
         {/* <AsideMenu toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} /> */}
         <Header_Donation link="/dashboard" />
         <section className="p-4 flex flex-col gap-8">
-          <h1 className=" -mb-4 font-semibold">Filtro de Busqueda:</h1>
-          <Formik
-            initialValues={initialValuesFilter}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmitFilterDonation}
-          >
-            <Form className="p-3 grid grid-cols-2 gap-4 rounded-r-xl rounded-md border-2 border-gray-500 shadow-xl overflow-hidden">
-              <div>
-                <label htmlFor="textoBusqueda" className="text-sm">Búsqueda</label>
-                <Field
-                  type="text"
-                  name="textoBusqueda"
-                  placeholder="Texto de búsqueda"
-                  className="px-3 w-[90%] rounded-md h-10 py-1 input-class outline-none border-1 border-solid border-black"
-                />
-                <ErrorMessage name="textoBusqueda" component="div" className="text-red-500 text-sm" />
-              </div>
-              <div>
-                <label htmlFor="edad" className="text-sm">Edad</label>
-                <Field
-                  type="number"
-                  name="edad"
-                  placeholder="Edad"
-                  className="px-3 w-[90%] rounded-md h-10 py-1 input-class outline-none border-1 border-solid border-black"
-                />
-                <ErrorMessage name="edad" component="div" className="text-red-500 text-sm" />
-              </div>
-              <div>
-                <label htmlFor="peso" className="text-sm">Peso</label>
-                <Field
-                  type="number"
-                  name="peso"
-                  placeholder="Peso"
-                  className="px-3 w-[90%] rounded-md h-10 py-1 input-class outline-none border-1 border-solid border-black"
-                />
-                <ErrorMessage name="peso" component="div" className="text-red-500 text-sm" />
-              </div>
-              <div>
-                <label htmlFor="altura" className="text-sm">Altura</label>
-                <Field
-                  type="number"
-                  name="altura"
-                  placeholder="Altura"
-                  className="input-class px-3 w-[90%] rounded-md h-10 py-1 input-class outline-none border-1 border-solid border-black"
-                />
-                <ErrorMessage name="altura" component="div" className="text-red-500 text-sm" />
-              </div>
-              <div className="flex flex-col">
-                <label htmlFor="generoOrdinal" className="text-sm">Género</label>
-                <Field as="select" name="generoOrdinal" className="input-class h-10 border-1 rounded-md border-gray-color">
-                  <option value="">Seleccionar</option>
-                  <option value="0">Masculino</option>
-                  <option value="1">Femenino</option>
-                  <option value="2">Otro</option>
-                </Field>
-                <ErrorMessage name="generoOrdinal" component="div" className="text-red-500 text-sm" />
-              </div>
-              <div className="flex flex-col">
-                <label htmlFor="factorSanguineoOrdinal" className="text-sm">Grupo RH</label>
-                <Field as="select" name="factorSanguineoOrdinal" className="input-class h-10 border-1 rounded-md border-gray-color">
-                  <option value="">Seleccionar</option>
-                  <option value="0">O+</option>
-                  <option value="1">O-</option>
-                  <option value="2">A+</option>
-                  <option value="3">A-</option>
-                  <option value="4">B+</option>
-                  <option value="5">B-</option>
-                  <option value="6">AB+</option>
-                  <option value="7">AB-</option>
-                </Field>
-                <ErrorMessage name="factorSanguineoOrdinal" component="div" className="text-red-500 text-sm" />
-              </div>
-              <div>
-                <label htmlFor="edadFiltro" className="text-sm">Filtro de Edad</label>
-                <Field as="select" name="edadFiltro" className="input-class h-10 border-1 rounded-md border-gray-color w-full">
-                  <option value="">Seleccionar</option>
-                  <option value="mayor">Mayor</option>
-                  <option value="menor">Menor</option>
-                  <option value="igual">Igual</option>
-                </Field>
-                <ErrorMessage name="edadFiltro" component="div" className="text-red-500 text-sm" />
-              </div>
-              <div>
-                <label htmlFor="pesoFiltro" className="text-sm">Filtro de Peso</label>
-                <Field as="select" name="pesoFiltro" className="input-class h-10 border-1 rounded-md border-gray-color w-full">
-                  <option value="">Seleccionar</option>
-                  <option value="mayor">Mayor</option>
-                  <option value="menor">Menor</option>
-                  <option value="igual">Igual</option>
-                </Field>
-                <ErrorMessage name="pesoFiltro" component="div" className="text-red-500 text-sm" />
-              </div>
-              <div>
-                <label htmlFor="alturaFiltro" className="text-sm">Filtro de Altura</label>
-                <Field as="select" name="alturaFiltro" className="input-class  h-10 border-1 rounded-md border-gray-color w-full">
-                  <option value="">Seleccionar</option>
-                  <option value="mayor">Mayor</option>
-                  <option value="menor">Menor</option>
-                  <option value="igual">Igual</option>
-                </Field>
-                <ErrorMessage name="alturaFiltro" component="div" className="text-red-500 text-sm" />
-              </div>
-              <button type="submit" className="h-10  col-span-2 w-full font-semibold rounded-md bg-secondary-brand-dark text-white">
-                Buscar
-              </button>
-            </Form>
-          </Formik>
+          <h1 className=" -mb-4 font-semibold flex justify-between items-center">Filtro de Busqueda: <span className=" w-10 h-10 rounded-full flex justify-center hover:brightness-75 transition-all duration-300 items-center bg-gray-200 border-2  cursor-pointer" onClick={() => setIsOpenFilter(!isOpenFilter)}><SilderIcon width={20} height={20} stroke="" /></span></h1>
+          {isOpenFilter &&
+            (<>
+              <Formik
+                initialValues={initialValuesFilter}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmitFilterDonation}
+              >
+                <Form className="p-3 grid grid-cols-2 gap-4 rounded-r-xl rounded-md border-2 border-gray-500 shadow-xl overflow-hidden">
+                  <div>
+                    <label htmlFor="textoBusqueda" className="text-sm">Búsqueda</label>
+                    <Field
+                      type="text"
+                      name="textoBusqueda"
+                      placeholder="Texto de búsqueda"
+                      className="px-3 w-[90%] rounded-md h-10 py-1 input-class outline-none border-1 border-solid border-black"
+                    />
+                    <ErrorMessage name="textoBusqueda" component="div" className="text-red-500 text-sm" />
+                  </div>
+                  <div>
+                    <label htmlFor="edad" className="text-sm">Edad</label>
+                    <Field
+                      type="number"
+                      name="edad"
+                      placeholder="Edad"
+                      className="px-3 w-[90%] rounded-md h-10 py-1 input-class outline-none border-1 border-solid border-black"
+                    />
+                    <ErrorMessage name="edad" component="div" className="text-red-500 text-sm" />
+                  </div>
+                  <div>
+                    <label htmlFor="peso" className="text-sm">Peso</label>
+                    <Field
+                      type="number"
+                      name="peso"
+                      placeholder="Peso"
+                      className="px-3 w-[90%] rounded-md h-10 py-1 input-class outline-none border-1 border-solid border-black"
+                    />
+                    <ErrorMessage name="peso" component="div" className="text-red-500 text-sm" />
+                  </div>
+                  <div>
+                    <label htmlFor="altura" className="text-sm">Altura</label>
+                    <Field
+                      type="number"
+                      name="altura"
+                      placeholder="Altura"
+                      className="input-class px-3 w-[90%] rounded-md h-10 py-1 input-class outline-none border-1 border-solid border-black"
+                    />
+                    <ErrorMessage name="altura" component="div" className="text-red-500 text-sm" />
+                  </div>
+                  <div className="flex flex-col">
+                    <label htmlFor="generoOrdinal" className="text-sm">Género</label>
+                    <Field as="select" name="generoOrdinal" className="input-class h-10 border-1 rounded-md border-gray-color">
+                      <option value="">Seleccionar</option>
+                      <option value="0">Masculino</option>
+                      <option value="1">Femenino</option>
+                      <option value="2">Otro</option>
+                    </Field>
+                    <ErrorMessage name="generoOrdinal" component="div" className="text-red-500 text-sm" />
+                  </div>
+                  <div className="flex flex-col">
+                    <label htmlFor="factorSanguineoOrdinal" className="text-sm">Grupo RH</label>
+                    <Field as="select" name="factorSanguineoOrdinal" className="input-class h-10 border-1 rounded-md border-gray-color">
+                      <option value="">Seleccionar</option>
+                      <option value="0">O+</option>
+                      <option value="1">O-</option>
+                      <option value="2">A+</option>
+                      <option value="3">A-</option>
+                      <option value="4">B+</option>
+                      <option value="5">B-</option>
+                      <option value="6">AB+</option>
+                      <option value="7">AB-</option>
+                    </Field>
+                    <ErrorMessage name="factorSanguineoOrdinal" component="div" className="text-red-500 text-sm" />
+                  </div>
+                  <div>
+                    <label htmlFor="edadFiltro" className="text-sm">Filtro de Edad</label>
+                    <Field as="select" name="edadFiltro" className="input-class h-10 border-1 rounded-md border-gray-color w-full">
+                      <option value="">Seleccionar</option>
+                      <option value="mayor">Mayor</option>
+                      <option value="menor">Menor</option>
+                      <option value="igual">Igual</option>
+                    </Field>
+                    <ErrorMessage name="edadFiltro" component="div" className="text-red-500 text-sm" />
+                  </div>
+                  <div>
+                    <label htmlFor="pesoFiltro" className="text-sm">Filtro de Peso</label>
+                    <Field as="select" name="pesoFiltro" className="input-class h-10 border-1 rounded-md border-gray-color w-full">
+                      <option value="">Seleccionar</option>
+                      <option value="mayor">Mayor</option>
+                      <option value="menor">Menor</option>
+                      <option value="igual">Igual</option>
+                    </Field>
+                    <ErrorMessage name="pesoFiltro" component="div" className="text-red-500 text-sm" />
+                  </div>
+                  <div>
+                    <label htmlFor="alturaFiltro" className="text-sm">Filtro de Altura</label>
+                    <Field as="select" name="alturaFiltro" className="input-class  h-10 border-1 rounded-md border-gray-color w-full">
+                      <option value="">Seleccionar</option>
+                      <option value="mayor">Mayor</option>
+                      <option value="menor">Menor</option>
+                      <option value="igual">Igual</option>
+                    </Field>
+                    <ErrorMessage name="alturaFiltro" component="div" className="text-red-500 text-sm" />
+                  </div>
+                  <button type="submit" disabled={loading} className="h-10 gap-1 flex justify-center items-center text-center  col-span-2 w-full font-semibold rounded-md bg-secondary-brand-dark text-white">
+                    <span className=" animate-spin">
+                      {loading && (
+                        <LoaderIcon width={30} height={30}></LoaderIcon>
+                      )}
+                    </span>
+                    Buscar
+                  </button>
+                </Form>
+              </Formik>
+            </>)
+          }
           <div className="rounded-r-xl rounded-xl border-2 border-gray-500 shadow-xl overflow-hidden">
             <div className="flex border-b-2 border-gray-500 rounded-lg mb-2 p-1 ">
               <button className="rounded-lg px-4 w-1/3 py-2 text-sm bg-[#5761C8] text-white border-solid border-[1px] border-gray-500">
@@ -213,29 +216,25 @@ export default function Donations() {
             </div>
             <ol>
               {donors == undefined ?
-                <p className=" text-center py-4">No hay donantes</p> :
-                donors.content.map((donor, idx) => (
-                  <li
-                    key={idx}
-                    onClick={() => console.log(donor.idMedico)}
-                    className="flex hover:bg-gray-200 transition-all duration-300 cursor-pointer justify-between py-1 px-2 border-b-1 border-gray-500">
-                    <div className="flex flex-row items-center w-full p-1">
-                      <div className="flex flex-col ml-3 w-full">
-                        <div className="flex flex-row justify-between">
-                          <p className="font-semibold text-sm">
-                            {donor.genero === 0 ? "Masculino" : donor.genero === 1 ? 'Femenino' : "Otro"} de{" "}
-                            {getAge(donor.fechaNacimiento)} años
-                          </p>
-                        </div>
-                        <p className="text-gray-700 text-sm">
-                          {donor.descripcion}
-                        </p>
-                      </div>
-                    </div>
-                  </li>
-                ))}
+                <p className=" text-center py-4">Completa el formulario para buscar donantes</p> :
+                <>
+                  {donors.content.length == 0 ?
+                    (<p className=" p-4 text-center">No se encontraron donantes que coincidan con los criterios de búsqueda</p>) : (
+                      donors.content.map((donor) => (
+                        <Donors idDonante={donor.idDonante}
+                          genero={donor.genero}
+                          fechaNacimiento={donor.fechaNacimiento}
+                          descripcion={donor.descripcion}>
+                        </Donors>
+                      ))
+                    )
+
+                  }
+                </>
+              }
             </ol>
           </div>
+
         </section>
 
       </div>
---
+++ frontend/src/pages/private/Medic_Pages/Donations/Donation_Home/Donors/Donors.tsx
@@ -0,0 +1,108 @@
+import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from '@nextui-org/react'
+import { getAge } from '../../../../../../utils/functions/functions'
+import React, { useState } from 'react'
+import { API_URL } from '../../../../../../api/api'
+import { SkeletonInput } from '../../../../../../components/Skeletons'
+
+interface DonorProps {
+  idDonante: number
+  genero: number
+  fechaNacimiento: string
+  descripcion: string
+}
+
+interface Medic {
+  nombre: string
+  apellido: string
+  telefono: string
+  provincia: string
+  localidad: string
+  licencia: string
+}
+
+
+export const Donors: React.FC<DonorProps> = ({ idDonante, genero, fechaNacimiento, descripcion }) => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [medic, setMedic] = useState<Medic>()
+  const [loading, setLoading] = useState(false)
+  const fetchDataMedicForDonor = async () => {
+    onOpen()
+    const token = localStorage.getItem('TOKEN_KEY');
+    setLoading(true)
+    try {
+      const res = await fetch(`${API_URL}/donante/buscar-medico-por-donante?idDonante=${idDonante}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      const data = await res.json()
+      setMedic(data)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+
+  return (
+    <>
+
+      <li
+        key={idDonante}
+        onClick={fetchDataMedicForDonor}
+        className="flex hover:bg-gray-200 transition-all duration-300 cursor-pointer justify-between py-1 px-2 border-b-1 border-gray-500">
+        <div className="flex flex-row items-center w-full p-1">
+          <div className="flex flex-col ml-3 w-full">
+            <div className="flex flex-row justify-between">
+              <p className="font-semibold text-sm">
+                {genero === 0 ? "Masculino" : genero === 1 ? 'Femenino' : "Otro"} de{" "}
+                {getAge(fechaNacimiento)} años
+              </p>
+            </div>
+            <p className="text-gray-700 text-sm">
+              {descripcion}
+            </p>
+          </div>
+        </div>
+      </li>
+
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} hideCloseButton={true} placement={'center'}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Contactar médico responsable</ModalHeader>
+              <ModalBody>
+                {loading ? <>
+                  <SkeletonInput label={'Nombre'} ></SkeletonInput>
+                  <SkeletonInput label={'Apellido'} ></SkeletonInput>
+                  <SkeletonInput label={'Telefono de contacto'} ></SkeletonInput>
+                  <SkeletonInput label={'Ubicación'} ></SkeletonInput>
+                </> : <>
+                  <label className=' font-semibold'>Nombre</label>
+                  <input className=' border-1 h-10 px-4 rounded-md border-gray-color' type="text" value={medic?.nombre} disabled />
+                  <label className=' font-semibold'>Apellido</label>
+                  <input className=' border-1 h-10 px-4 rounded-md border-gray-color' type="text" value={medic?.apellido} disabled />
+                  <label className=' font-semibold'>Telefono de contacto</label>
+                  <input className=' border-1 h-10 px-4 rounded-md border-gray-color' type="text" value={medic?.telefono} disabled />
+                  <label className=' font-semibold'>Ubicación</label>
+                  <input className=' border-1 h-10 px-4 rounded-md border-gray-color' type="text" value={`${medic?.provincia}, ${medic?.localidad}`} disabled />
+                </>
+
+                }
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" className=' border-1 border-red-300' variant="light" onPress={onClose}>
+                  Cerrar
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
