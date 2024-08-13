8ad177c32b9e9c9b5b186439a3a4091f7896dcf3
Facundo Pettersson <petterssonfacundo@gmail.com>
2024-08-13 13:56:15 -0300

    Add: ConectionAdministrador

--- frontend/src/components/Components_Admin/Medicament_Admin/Medicament_Admin.tsx
+++ frontend/src/components/Components_Admin/Medicament_Admin/Medicament_Admin.tsx
@@ -40,6 +40,8 @@ export function Medicament_Admin() {
         }
     }
 
+    console.log(mostrarLoader)
+
     useEffect(() => {
         const timer = setTimeout(() => {
           setMostrarLoader(true);
@@ -129,7 +131,7 @@ export function Medicament_Admin() {
                     <>
                         <h2 className="font-inter font-bold text-xl ml-2 mb-5">Listado</h2>
                         <section className="mb-2 w-full shadow-custom-right rounded-xl border-1 border-solid border-orange-600 py-2 px-1">
-                            {info.content.slice(0, 4).map((institution, index) => (
+                            {info.content.slice(0, 4).map((institution) => (
                                 <div key={institution.idMedicamento} className="flex flex-row items-center pl-2 pt-2 border-b-3 border-orange-400 rounded-xl pb-4 mt-2">
                                     <Medicament_admin width={66} height={66} classname="rounded-lg bg-orange-600 p-2" stroke="#fff" />
                                     <div className="ml-2">
