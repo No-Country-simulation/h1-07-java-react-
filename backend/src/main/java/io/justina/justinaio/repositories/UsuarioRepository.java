db2700cb81e947d4b7b56b606c0ce61ad3903825
PabloMagna <pablo.magnavachi@alumnos.frgp.utn.edu.ar>
2024-08-16 19:30:42 -0300

    feat: cambios en security

--- backend/src/main/java/io/justina/justinaio/repositories/UsuarioRepository.java
+++ backend/src/main/java/io/justina/justinaio/repositories/UsuarioRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
     Optional<Usuario> findByEmail(String username);
-    Optional<Usuario> findByUsernameee(String username);
+    Optional<Usuario> findByUsernamee(String username);
 }
